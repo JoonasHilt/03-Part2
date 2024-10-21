b29ba11a71edc5f1b5f666e43ab22391f30d948a
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-10-21 10:58:57 +0300

    aloitettu tehtävä 2.1 ennen lounasta

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import Note from "./components/Note";
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
 
-const App = ({ notes }) => {
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      <Course course={course} />
     </div>
   );
 };
