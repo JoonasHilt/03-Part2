117cf62b4ba3b448fcaf54b07ac966b84e9ccb5d
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-10-21 11:05:54 +0300

    palautettu viimeisin toimiva versio ennen tehtävää 2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+import Note from "./components/Note";
 
+const App = ({ notes }) => {
   return (
     <div>
-      <Course course={course} />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
   );
 };
